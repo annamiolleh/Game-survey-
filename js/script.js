d488f7e26c22676324ae7273bdc7a6d5e81e8a4e
annamiolleh <anka301493@gmail.com>
2024-10-07 13:10:20 +0300

    scrutinizing v-bind directive for inserting variables in HTML code alongside crafting dynamically changed result page

--- js/script.js
+++ js/script.js
@@ -24,8 +24,8 @@ let app = new Vue({
             'infested': 0,
             'hybrid': 0,
         },
-        totalGames: localStorage.getItem('sc2TotalGame') ? localStorage.getItem
-        ('sc2TotalGame') : 0,
+        totalGames: localStorage.getItem('sc2TotalGames') ? localStorage.getItem
+        ('sc2TotalGames') : 0,
         questions: questions,
         results: results,
         resultRace: 'infested',
@@ -81,11 +81,61 @@ let app = new Vue({
         nextQuestions(answer) {
             if(this.number == 24) {
                 this.number = 0
-                //this.endGame();
+                this.endGame();
             } else {
                 this.number++
             }
             eval(answer)
+        },
+        endGame() {
+            this.totalGames++;
+            localStorage.setItem('sc2TotalGames', this.totalGames)
+            //zerg
+            if (this.score.zerg > this.score.protoss && this.score.zerg > this.score.terran && 
+                this.score.primal < 8 && Math.abs(this.score.protoss - this.score.zerg) > 3) {
+                    this.goToResult('zerg');
+                    this.totalGame.zerg++;
+                }
+            //primal zerg
+            else if (this.score.primal > this.score.protoss && 
+                this.score.primal > this.score.terran &&
+                this.score.primal == 8) {
+                    this.goToResult('primal');
+                    this.totalGame.primal++;
+                }
+            //protoss
+            else if (this.score.protoss > this.score.zerg && 
+                this.score.protoss > this.score.terran &&
+                this.score.taldarim < 5 &&
+                Math.abs(this.scrore.protoss - this.score.zerg) > 3) {
+                    this.goToResult('protoss');
+                    this.totalGame.protoss++;
+                }
+            //taldarim
+            else if (this.score.protoss > this.score.zerg && 
+                this.score.protoss > this.score.terran &&
+                this.score.taldarim == 5) {
+                    this.goToResult('taldarim');
+                    this.totalGame.taldarim++;
+                }
+            //terran
+            else if (this.score.terran > this.score.zerg && 
+                this.score.terran > this.score.protoss) {
+                    this.goToResult('terran');
+                    this.totalGame.terran++;
+                }
+            //hybrid
+            else if (Math.abs(this.score.protoss - this.score.zerg) <=3) {
+                    this.goToResult('hybrid');
+                    this.totalGame.hybrid++;
+                }
+            // infested
+            else {
+                this.goToResult('infested');
+                this.totalGame.infested++;
+            }
+            
+            localStorage.setItem('sc2TotalGame', JSON.stringify(this.totalGame));
         }
     }
 })
